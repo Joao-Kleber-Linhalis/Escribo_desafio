e78fb96b7a74fe6b1a813c87d62bb25348b73b6f
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2023-11-17 23:41:55 -0300

    Desafio 1

---
+++ lib/uteis/widgets/botao.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Botao extends StatelessWidget {
+  String texto;
+  VoidCallback? ao_clicar;
+  FocusNode? marcador_foco;
+  Color? cor;
+  bool mostrar_progress;
+
+  Botao(
+      {required this.texto,
+       required this.ao_clicar,
+      this.marcador_foco,
+      this.cor,
+      this.mostrar_progress = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(10.0)),
+              backgroundColor: cor,
+        ),
+        onPressed: ao_clicar,
+        focusNode: marcador_foco,
+        child: mostrar_progress
+            ? const Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              )
+            : Text(
+                texto,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+      ),
+    );
+  }
+}
